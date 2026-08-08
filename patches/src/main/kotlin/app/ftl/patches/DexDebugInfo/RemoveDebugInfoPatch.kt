7a6493d466b8f6ae62b7287639ef9c19297c3fa1
BlazeFTL <139938337+BlazeFTL@users.noreply.github.com>
2026-08-09 00:38:59 +0530

    feat: up

--- patches/src/main/kotlin/app/ftl/patches/DexDebugInfo/RemoveDebugInfoPatch.kt
+++ patches/src/main/kotlin/app/ftl/patches/DexDebugInfo/RemoveDebugInfoPatch.kt
@@ -1,5 +1,7 @@
 package your.patches.category
 
+import app.morphe.patcher.patch.bytecodePatch
+import com.android.tools.smali.dexlib2.builder.MutableMethodImplementation
 import com.android.tools.smali.dexlib2.iface.debug.EndLocal
 import com.android.tools.smali.dexlib2.iface.debug.LineNumber
 import com.android.tools.smali.dexlib2.iface.debug.PrologueEnd
@@ -9,33 +11,33 @@ import com.android.tools.smali.dexlib2.iface.debug.StartLocal
 
 val removeDebugInfoPatch = bytecodePatch(
     name = "Remove debug info",
-    description = "Removes debug info (.source, .line, .local, .param, .prologue) from every class in all classes*.dex files.",
+    description = "Removes debug info (.source, .line, .local, .prologue) from every class in all classes*.dex files.",
     default = true
 ) {
-    val removeAll by booleanOption(name = "Remove all debug info", default = true)
-    val removeSource by booleanOption(name = "Remove .source", default = true)
-    val removeLine by booleanOption(name = "Remove .line", default = true)
-    val removeParam by booleanOption(name = "Remove .param", default = true)
-    val removePrologue by booleanOption(name = "Remove .prologue", default = true)
-    val removeLocal by booleanOption(name = "Remove .xxx local", default = true)
+    val removeAll by booleanOption(key = "removeAll", default = true, title = "Remove all debug info")
+    val removeSource by booleanOption(key = "removeSource", default = true, title = "Remove .source")
+    val removeLine by booleanOption(key = "removeLine", default = true, title = "Remove .line")
+    val removePrologue by booleanOption(key = "removePrologue", default = true, title = "Remove .prologue")
+    val removeLocal by booleanOption(key = "removeLocal", default = true, title = "Remove .xxx local")
 
     execute {
-        classes.forEach { classDef ->
+        classDefForEach { classDef ->
             val mutableClass = mutableClassDefBy(classDef)
 
             mutableClass.methods.forEach { method ->
-                if (removeAll || removeParam) {
-                    method.parameters.forEach { it.name = null }
-                }
+                val impl = method.implementation as? MutableMethodImplementation ?: return@forEach
 
-                val impl = method.implementation ?: return@forEach
+                val locations = impl.instructions.map { it.location } +
+                    impl.newLabelForIndex(impl.instructions.size).location
 
-                impl.debugItems.removeAll { item ->
-                    removeAll ||
-                        (removeSource && item is SetSourceFile) ||
-                        (removeLine && item is LineNumber) ||
-                        (removePrologue && item is PrologueEnd) ||
-                        (removeLocal && (item is StartLocal || item is EndLocal || item is RestartLocal))
+                locations.forEach { location ->
+                    location.debugItems.removeIf { item ->
+                        removeAll == true ||
+                            (removeSource == true && item is SetSourceFile) ||
+                            (removeLine == true && item is LineNumber) ||
+                            (removePrologue == true && item is PrologueEnd) ||
+                            (removeLocal == true && (item is StartLocal || item is EndLocal || item is RestartLocal))
+                    }
                 }
             }
         }
