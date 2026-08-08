e3030716c74c1f59d29c579d307386aa91e891ab
BlazeFTL <139938337+BlazeFTL@users.noreply.github.com>
2026-08-08 13:08:54 +0530

    Update RemoveAdsPatch.kt

--- patches/src/main/kotlin/app/ftl/patches/ads/RemoveAdsPatch.kt
+++ patches/src/main/kotlin/app/ftl/patches/ads/RemoveAdsPatch.kt
@@ -2,7 +2,13 @@ package app.ftl.patches.ads
 
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
+import app.morphe.patcher.extensions.InstructionExtensions.instructionsOrNull
+import app.morphe.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.morphe.patcher.patch.bytecodePatch
+import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.reference.StringReference
 
 internal object AdViewLoadAdFingerprint : Fingerprint(
     name = "loadAd",
@@ -58,9 +64,148 @@ internal object IronSourceLoadInterstitialFingerprint : Fingerprint(
     returnType = "V",
 )
 
+internal val AD_STRING_BLACKLIST = listOf(
+    "61.145.124.238",
+    "ad.api.kaffnet",
+    "ad.mail.ru",
+    "ad.myinstashot.com",
+    "adbuddiz",
+    "adc3-launch",
+    "adcolony",
+    "addapptr",
+    "adincube",
+    "adjust",
+    "adkmob",
+    "adknowledge",
+    "admarvel",
+    "admob",
+    "Admob",
+    "admost",
+    "adnw_logging",
+    "adsafeprotected",
+    "adsdk",
+    "adsert",
+    "adserver",
+    "adservice",
+    "advertising",
+    "adview",
+    "adz.wattpad",
+    "aerserv",
+    "airpush",
+    "alta.eqmob",
+    "altamob",
+    "amazon-adsystem",
+    "amazonaws",
+    "analytics",
+    "appAdForce",
+    "appboy",
+    "appbrain",
+    "appenda",
+    "appia",
+    "applifier.com",
+    "applovin",
+    "applvn",
+    "appnext",
+    "appnexus",
+    "appodeal",
+    "apprupt",
+    "appsdt",
+    "appsflyer",
+    "apsalar",
+    "audience_network",
+    "avocarrot",
+    "azure",
+    "boxdigital/sdk/ad",
+    "branch",
+    "ca-app-pub",
+    "certificate.mobile.yandex.net",
+    "chartboost",
+    "cloudfront",
+    "code.google.com/p/android/issues/detail",
+    "crashlytics",
+    "csi.gstatic.com",
+    "doubleclick.net",
+    "dsp.batmobil",
+    "duapps",
+    "firebaseapp",
+    "flurry",
+    "fyber",
+    "g.doubleclick",
+    "google.com/safebrowsing/clientreport",
+    "google/android/gms/internal",
+    "googleads",
+    "googleapis.com/auth/games",
+    "googlesyndication",
+    "graph.facebook",
+    "greystripe",
+    "heyzap",
+    "hockeyapp",
+    "hyprmx",
+    "InlineAd",
+    "inmobi",
+    "inneractive",
+    "instreamatic",
+    "integralads",
+    "ironsource",
+    "jirbo",
+    "jumptap",
+    "kochava",
+    "Leadbolt",
+    "localytics",
+    "loopme",
+    "madnet.ru",
+    "mdotm",
+    "measurement",
+    "mediabrix",
+    "metrica",
+    "millennialmedia",
+    "mngads",
+    "moat",
+    "mobclix",
+    "mobfox",
+    "mobvista",
+    "montexi",
+    "moolah",
+    "mopub",
+    "mp.mydas.mobi",
+    "my/target",
+    "NativeInterstitial",
+    "net.rayjump",
+    "network_ads_common",
+    "nexage",
+    "onelouder/adlib",
+    "openx",
+    "pagead/ads",
+    "plus1.wapstart.ru",
+    "pubmatic",
+    "pubnative",
+    "r.my.com/mobile",
+    "revmob",
+    "sb.scorecardresearch",
+    "smaato/SOMA",
+    "startapp",
+    "startup.mobile.yandex.net",
+    "supersonicads",
+    "tagmanager",
+    "tapas",
+    "tapjoy",
+    "udm.scorecardresearch",
+    "unity3d/ads",
+    "unityads",
+    "vdopia",
+    "vungle",
+    "www.dummy",
+    "wzrkt",
+    "xtify",
+    "yandexadexchange",
+    "zestadz",
+)
+
+private const val AD_STRING_REPLACEMENT = "Remove-Fucking-Ads"
+
 val removeAdsPatch = bytecodePatch(
     name = "Remove ads",
-    description = "Neuters ad-load entry points for major ad SDKs and hides leftover ad view containers in layout XML.",
+    description = "Neuters ad-load entry points for major ad SDKs, poisons const-string ad network hosts/unit-id prefixes across all bytecode, and hides leftover ad view containers in layout XML.",
 ) {
     dependsOn(hideAdLayoutsPatch)
 
@@ -74,5 +219,28 @@ val removeAdsPatch = bytecodePatch(
         AppLovinMaxRewardedLoadFingerprint.methodOrNull?.addInstructions(0, "return-void")
         UnityAdsLoadFingerprint.methodOrNull?.addInstructions(0, "return-void")
         IronSourceLoadInterstitialFingerprint.methodOrNull?.addInstructions(0, "return-void")
+
+        classes.forEach { classDef ->
+            val hasMatch = classDef.methods.any { method ->
+                (method.instructionsOrNull ?: emptyList()).any { instruction ->
+                    instruction.opcode == Opcode.CONST_STRING &&
+                        AD_STRING_BLACKLIST.any { term ->
+                            ((instruction as ReferenceInstruction).reference as StringReference)
+                                .string.contains(term, ignoreCase = true)
+                        }
+                }
+            }
+            if (!hasMatch) return@forEach
+
+            mutableClassDefBy(classDef).methods.forEach { method ->
+                (method.instructionsOrNull ?: emptyList()).forEachIndexed { index, instruction ->
+                    if (instruction.opcode != Opcode.CONST_STRING) return@forEachIndexed
+                    val value = ((instruction as ReferenceInstruction).reference as StringReference).string
+                    if (AD_STRING_BLACKLIST.none { value.contains(it, ignoreCase = true) }) return@forEachIndexed
+                    val register = (instruction as OneRegisterInstruction).registerA
+                    method.replaceInstruction(index, "const-string v$register, \"$AD_STRING_REPLACEMENT\"")
+                }
+            }
+        }
     }
 }
