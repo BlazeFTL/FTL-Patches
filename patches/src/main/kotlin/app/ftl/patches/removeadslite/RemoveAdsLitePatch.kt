5ec7c89cd1617bf86356864ee0b3ad7fdd3fe8bb
BlazeFTL <139938337+BlazeFTL@users.noreply.github.com>
2026-08-23 00:26:25 +0530

    Update RemoveAdsLitePatch.kt

--- patches/src/main/kotlin/app/ftl/patches/removeadslite/RemoveAdsLitePatch.kt
+++ patches/src/main/kotlin/app/ftl/patches/removeadslite/RemoveAdsLitePatch.kt
@@ -28,7 +28,7 @@ import java.util.logging.Logger
 // RemoveAdsPatch can cause.
 val removeAdsLitePatch = bytecodePatch(
     name = "Remove Ads Lite(Adobo)",
-    description = "Based On (Adobo's Block Ads+Mobile Ads) Disables known ad SDK entry points and neutralizes ad/tracker/analytics " +
+    description = "Based On (Adobo's Block Ads+Mobile Ads) It Should Work Where Remove Ads Patch Failed " +
         "Safer than Remove Ads for apps where you will be stuck in SplashActivity " +
         "It Is Weaker But Effective, Merged Adobo's Both Patches And Removed The Need Of Selecting A Host File Or Configuring Anything " +
         "In Future It May Replace Remove Ads Patch If I Find No Problem.",
