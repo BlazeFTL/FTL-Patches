6a859bea8fc6273efda044c71ee08a3c284a68c8
BlazeFTL <139938337+BlazeFTL@users.noreply.github.com>
2026-09-13 20:03:30 +0530

    Update Constants.kt

--- patches/src/main/kotlin/app/ftl/patches/ImpoStack/Constants.kt
+++ patches/src/main/kotlin/app/ftl/patches/ImpoStack/Constants.kt
@@ -7,7 +7,6 @@ internal val COMPATIBILITY_IMPOSTACK = Compatibility(
     packageName = "com.bytedz.printflow",
     name = "ImpoStack",
     targets = listOf(
-        // versionName / versionCode as given directly (1.4.0 / 10).
         AppTarget(version = "1.4.0", versionCode = 10),
     ),
 )
