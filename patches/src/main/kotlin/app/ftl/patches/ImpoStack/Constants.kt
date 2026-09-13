09eeeeee619c9372d0714d1ba60240c3f46c7e30
BlazeFTL <139938337+BlazeFTL@users.noreply.github.com>
2026-09-13 20:21:35 +0530

    Update Constants.kt

--- patches/src/main/kotlin/app/ftl/patches/ImpoStack/Constants.kt
+++ patches/src/main/kotlin/app/ftl/patches/ImpoStack/Constants.kt
@@ -5,7 +5,7 @@ import app.morphe.patcher.patch.Compatibility
 
 internal val COMPATIBILITY_IMPOSTACK = Compatibility(
     packageName = "com.bytedz.printflow",
-    name = "ImpoStack",
+    name = "ImpoStack: PDF Imposition",
     targets = listOf(
         AppTarget(version = "1.4.0", versionCode = 10),
     ),
